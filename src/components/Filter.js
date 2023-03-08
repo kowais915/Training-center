5de1a3f16aad471cc3065cbab7ea681c6b47416c
owais <Owais>
2023-03-09 04:41:40 +0500

    added the dropdown and the checkbox to the filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {filter} from '../styles/filter.module.css';
+import {filter, status} from '../styles/filter.module.css';
 
 const Filter = () => {
     return (  
         <div className={filter}>
-                <h1>Filter</h1>
+                <div className={status}>
+                    <label htmlFor="dropdown">Status</label>
+                    <select name="status" id="dropdown">
+                        <option value="stable">stable</option>
+                        <option value="beta">beta</option>
+                        <option value="alpha">alpha</option>
+                    </select>
+
+                </div>
+
+
+                <div className={status}>
+                    <label htmlFor="checkbox">Videos</label>
+                    <input type="checkbox" id='checkbox' name='videos'/>
+
+                </div>
         </div>
     );
 }
