fa295c7ecc561e6072ac8a83796084754d6214c7
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-09 18:17:10 +0500

    working on the status filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {filter, status} from '../styles/filter.module.css';
+import {graphql, useStaticQuery } from 'gatsby';
 
-const Filter = () => {
+const Filter = ({filterChange, item}) => {
+
+    const [selectedItem, setSelectedItem ] = useState("all");
+
+    const filters = [
+        {label: "stable ", value: "stable "}, 
+        {label: "alpha ", value: "alpha "}, 
+        {label: "beta ", value: "beta"},
+        {label: "all ", value: "all"}
+    ];
+
+
+// a function that handles if user selects a different value from the dropdown
+
+const dropHandle = (event )=>{
+    setSelectedItem(event.target.value)
+}
+
     return (  
         <div className={filter}>
                 <div className={status}>
                     <label htmlFor="dropdown">Status</label>
-                    <select name="status" id="dropdown">
-                        <option value="stable">stable</option>
-                        <option value="beta">beta</option>
-                        <option value="alpha">alpha</option>
+                    <select name="status" value={selectedItem} onChange={dropHandle} id="dropdown">
+                        <option value="">Select</option>
+
+
+
+                        {filters.map(({label, value})=>(
+                            <option value={value} key={value}>{label}</option>
+                        ))}
+                        
+                       
                     </select>
 
                 </div>
 
 
-                <div className={status}>
+                {/* <div className={status}>
                     <label htmlFor="checkbox">Videos</label>
                     <input type="checkbox" id='checkbox' name='videos'/>
 
-                </div>
+                </div> */}
+
+
         </div>
     );
 }
--- src/components/TrainingModule.js
+++ src/components/TrainingModule.js
@@ -2,15 +2,18 @@ import React from 'react';
 import {Link } from 'gatsby';
 import { train } from '../styles/training.module.css';
 
-const TrainingModule = ({title, description, repoLink, pageLink, videoLink}) => {
+const TrainingModule = ({title, status, description, repoLink, pageLink, videoLink}) => {
     return ( 
-            <div className={train}>
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, cum. Ipsa aspernatur placeat voluptatibus aliquid tempora assumenda quidem eius ratione!</p>
+            <div className={train} >
+                <h2>{title}</h2>
+                <p>{description}</p>
 
                 <Link to={repoLink}>Github</Link>
                 <Link to={pageLink}>Website</Link>
                 <Link to={videoLink}>Watch Video</Link>
+                <div>
+                    <h5>{status}</h5>
+                </div>
 
             </div>
 
