93daf5f25809d835b6a184f5c8beed3c8861238d
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-10 00:59:14 +0500

    added icons and styled them

--- src/components/TrainingModule.js
+++ src/components/TrainingModule.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link } from 'gatsby';
-import { train, stable, stableText } from '../styles/training.module.css';
+import { FaGithub, FaGlobe, FaYoutube } from 'react-icons/fa';
+import { train, stable, stableText, github, alpbeta, icon } from '../styles/training.module.css';
 
 const TrainingModule = ({title, status, description, repoLink, pageLink, videoLink}) => {
    
@@ -13,16 +14,14 @@ const TrainingModule = ({title, status, description, repoLink, pageLink, videoLi
     return ( 
             <div className={statusBool ? stable: train}>
 
-                {statusBool ? <p className={stableText}>Stable </p> : null }
+                {statusBool ? <p className={stableText}>Stable </p> : <p className={alpbeta}>{status}</p> }
                 <h2>{title}</h2>
                 <p>{description}</p>
 
-                <Link to={repoLink}>Github</Link>
-                <Link to={pageLink}>Website</Link>
-                <Link to={videoLink}>Watch Video</Link>
-                <div>
-                    <h5>{status}</h5>
-                </div>
+                <Link className={github} to={repoLink}> <FaGithub className={icon} size={20} color="black" /> Github</Link>
+                <Link className={github} to={pageLink}> <FaGlobe className={icon} size={20} color="black"/>  Website</Link>
+                <Link className={github} to={videoLink}><FaYoutube className={icon}  size={20} color="red"/>Video</Link>
+               
 
             </div>
 
